a1f75b37c91c2cf78a131b864432f1a3f02a0b92
EL MUSTAPHA LAKHLOUFI <111161472+MstafaLkhloufi@users.noreply.github.com>
2023-01-23 17:06:48 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TimerManager from './components/TimerManager'
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <TimerManager />
+     
+
+    </div>
+  );
+}
+
+export default App;
